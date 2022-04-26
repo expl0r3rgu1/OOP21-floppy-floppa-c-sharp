321fa461bbe94491e14d70e8c35f04510cb1f3ea
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-04-27 00:21:13 +0200

    Created Character class

---
+++ CristinaZoccola/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP21_floppy_floppa_c_sharp.CristinaZoccola
+{
+    class Character
+    {
+        public Character()
+        {
+
+        }
+    }
+}
