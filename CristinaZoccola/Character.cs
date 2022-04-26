521dc68c01b8345d6d407633abaa571a7cab34af
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-04-27 00:50:06 +0200

    Added fields and constructor

--- CristinaZoccola/Character.cs
+++ CristinaZoccola/Character.cs
@@ -1,14 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Windows.Forms;
+using System.Timers;
+using System;
 
 namespace OOP21_floppy_floppa_c_sharp.CristinaZoccola
 {
-    class Character
+    class Character : Movable
     {
-        public Character()
-        {
+        public static bool immortal = false;
+        private readonly Skin _skin;
+        private bool _dead;
+        private bool _jumping;
+        private readonly Timer _timer;
 
+        public Character(Position position, Skin skin) : base(position)
+        {
+            _skin = skin;
+            _dead = false;
+            _jumping = false;
+            _timer = new Timer(500);
+            _timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
         }
+
+        public override void Animate(RibbonElementPaintEventArgs ribbonPaintEventArgs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            _jumping = false;
+            _timer.Stop();
+        }
+
     }
 }
