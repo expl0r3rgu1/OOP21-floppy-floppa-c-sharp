b3afc3df3200c983f04f1a5c0f6fb9b6af48220c
expl0r3rgu1 <salvaddimando@gmail.com>
2022-04-26 20:21:00 +0200

    Created Player class

---
+++ SalvatoreAntonioAddimando/Player.cs
@@ -0,0 +1,21 @@
+﻿public class Player
+{
+	private const string nickname;
+	private int personalBest;
+
+	public string Nickname
+    {
+        get { return nickname; }
+    }
+
+	public int PersonalBest
+    {
+		get { return personalBest; }
+		set { personalBest = value; }
+    }
+
+	public Player(string nickname)
+	{
+		this.nickname = nickname;
+	}
+}
