e09895f9da018649974c4f9030648ba48cb06d97
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-04-26 22:32:52 +0200

    Added comments

--- CristinaZoccola/Movable.cs
+++ CristinaZoccola/Movable.cs
@@ -2,18 +2,35 @@
 
 namespace OOP21_floppy_floppa_c_sharp.CristinaZoccola 
 {
+    /// <summary>
+    /// A class that represents all entities that will move on the map
+    /// </summary>
 	public abstract class Movable
 	{
 	    private Position _position;
 
+        /// <param name="position">the initial spawning position of the entity</param>
         public Movable(Position position) => _position = position;
 
+        /// <summary>
+        /// Getter of the entity position
+        /// </summary>
+        /// <returns>the current position of the entity</returns>
 	    public Position GetPosition() => _position;
 
+        /// <summary>
+        /// Setter of the entity position
+        /// </summary>
+        /// <param name="position">the new position of the entity</param>
 	    public void SetPosition(Position position) => _position = position;
 
+        /// <summary>
+        /// A method that will be used to animate all the entities on the map
+        /// </summary>
+        /// <param name="paintEventArgs">the object that will draw the entities on the map</param>
 	    public abstract void Animate(PaintEventArgs paintEventArgs);
 
+        /// <inheritdoc />
         public override bool Equals(object obj)
         {
             Movable other = (Movable)obj;
@@ -26,6 +43,7 @@ namespace OOP21_floppy_floppa_c_sharp.CristinaZoccola
             return _position.Equals(other.GetPosition);
         }
 
+        /// <inheritdoc />
         public override int GetHashCode()
         {
             return base.GetHashCode();
