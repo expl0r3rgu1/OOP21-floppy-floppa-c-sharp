3b345e2edb463618dc748ab8254d85fe77dfb932
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-27 00:49:43 +0200

    changed FixedObstacle

--- ValentinaPieri/FixedObstacle.cs
+++ ValentinaPieri/FixedObstacle.cs
@@ -1,35 +1,42 @@
 ﻿using System;
 
-public class FixedObstacle
+namespace ObstacleFactory
 {
-	private Image _skin;
-	private int _x;
-	private int _y;
-	public const int MovingFactor = 2;
-
-	public FixedObstacle(int x, int y, Image skin)
+	public class FixedObstacle
 	{
-		_x = x;
-		_y = y;
-		_skin = skin;
+		private Image _skin;
+		private int _x;
+		private int _y;
+		public const int MovingFactor = 2;
+
+		public FixedObstacle(int x, int y, Image skin)
+		{
+			_x = x;
+			_y = y;
+			_skin = skin;
+		}
+
+		public GetSkin() => _skin;
+
+		private void updatePosition()
+		{
+			_x = _x - MovingFactor;
+			_y = _y;
+		}
+
+		public override bool equals(object obj)
+		{
+			FixedObstacle other = (FixedObstacle)obj;
+
+			if (other == null)
+			{
+				return false;
+			}
+
+			return _x.Equals(other._x) & _y.Equals(other._y) & _skin.Equals(other.GetSkin);
+		}
+
+		public override int GetHashCode() => base.GetHashCode();
 	}
 
-	public GetSkin() => skin;
-
-	private void updatePosition()
-	{
-		_x = _x - MovingFactor;
-		_y = _y;
-	}
-
-	public override bool equals(object obj)
-	{
-		FixedObstacle other = (FixedObstacle)obj;
-		return this._x == other._x & this._y == other._y & this._skin = other._skin;
-	}
-
-    public override int GetHashCode()
-    {
-        return base.GetHashCode();
-    }
 }
