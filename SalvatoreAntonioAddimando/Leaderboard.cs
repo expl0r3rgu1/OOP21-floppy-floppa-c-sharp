ed4248b161d3256fd9d19adbbedc1264d92b5f89
expl0r3rgu1 <salvaddimando@gmail.com>
2022-04-26 21:21:23 +0200

    Created fields in Leaderboard

---
+++ SalvatoreAntonioAddimando/Leaderboard.cs
@@ -0,0 +1,10 @@
+class Leaderboard
+{
+    private const string leaderboardFilePath = "savings";
+    private ArrayList<Player> leaderboard;
+
+    public ArrayList<Player> Leaderboard
+    {
+        get { return leaderboard; }
+    }
+}
