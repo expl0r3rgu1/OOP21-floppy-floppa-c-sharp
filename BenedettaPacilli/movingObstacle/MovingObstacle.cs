4ab5c62da71d1faa898fb1d0fe4f908acee683a6
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-26 22:08:46 +0200

    Added updatePosition method

--- BenedettaPacilli/movingObstacle/MovingObstacle.cs
+++ BenedettaPacilli/movingObstacle/MovingObstacle.cs
@@ -1,5 +1,6 @@
 using System.Timers;
 using System.Drawing;
+using System;
 
 public class MovingObstacle
 {
@@ -8,6 +9,8 @@ public class MovingObstacle
 	private int _x;
 	private int _y;
 	private int _direction = -1;
+	public const int MovingFactor = 2;
+		//(int) Math.floor(SCREEN_SIZE.getWidth() / (double)1000) * 2;
 
 	public MovingObstacle(int x, int y, Image skin) //Position and Skin
 	{
@@ -25,5 +28,11 @@ public class MovingObstacle
 	public GetSkin()
     {
 		return _skin;
+    }
+
+	private void _updatePosition()
+    {
+		_x = _x - 3 * MovingFactor;
+		_y = _y + _direction * MovingFactor;
     }
 }
