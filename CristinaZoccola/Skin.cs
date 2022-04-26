156d8a95e164856154d269d4ff53411c3bd988d3
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-04-26 23:11:44 +0200

    Added fields and created constructor

--- CristinaZoccola/Skin.cs
+++ CristinaZoccola/Skin.cs
@@ -4,8 +4,17 @@ namespace OOP21_floppy_floppa_c_sharp.CristinaZoccola
 {
 	public class Skin
 	{
-		public Skin()
+		private string _name;
+		private Image _image;
+		private int _width;
+		private int _height;
+
+		public Skin(string name, Image image, int width, int height)
 		{
+			_name = name;
+			_image = image;
+			_width = width;
+			_height = height;
 		}
 	}
 }
