d5cc918cb952d9aecdd1f651ce192b1b4d613356
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-04-26 22:28:09 +0200

    Added Equals() and GetHashCode()

--- CristinaZoccola/Movable.cs
+++ CristinaZoccola/Movable.cs
@@ -4,15 +4,31 @@ namespace OOP21_floppy_floppa_c_sharp.CristinaZoccola
 {
 	public abstract class Movable
 	{
-	private Position _position;
+	    private Position _position;
 
-    public Movable(Position position) => _position = position;
+        public Movable(Position position) => _position = position;
 
-	public Position GetPosition() => _position;
+	    public Position GetPosition() => _position;
 
-	public void SetPosition(Position position) => _position = position;
+	    public void SetPosition(Position position) => _position = position;
 
-	public abstract void Animate(PaintEventArgs paintEventArgs);
+	    public abstract void Animate(PaintEventArgs paintEventArgs);
 
-	}
+        public override bool Equals(object obj)
+        {
+            Movable other = (Movable)obj;
+
+            if(other == null)
+            {
+                return false;
+            }
+
+            return _position.Equals(other.GetPosition);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
 }
