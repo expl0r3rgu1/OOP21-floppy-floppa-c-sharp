1a78f518847a4251c5a5ba5fec6f2863d52202e2
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-27 00:30:58 +0200

    added class FixedObstacle

---
+++ ValentinaPieri/FixedObstacle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class FixedObstacle
+{
+	private Image _skin;
+	private int _x;
+	private int _y;
+	public const int MovingFactor = 2;
+
+	public FixedObstacle(int x, int y, Image skin)
+	{
+		_x = x;
+		_y = y;
+		_skin = skin;
+	}
+
+	public GetSkin() => skin;
+
+	private void updatePosition()
+	{
+		_x = _x - MovingFactor;
+		_y = _y;
+	}
+
+	public override bool equals(object obj)
+	{
+		FixedObstacle other = (FixedObstacle)obj;
+		return this._x == other._x & this._y == other._y & this._skin = other._skin;
+	}
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
