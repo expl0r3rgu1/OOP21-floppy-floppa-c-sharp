0ad2f3d42538e84f34cdbcc3ed805fe1f7442f54
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-26 21:48:57 +0200

    Added MovingObstacle fields

---
+++ BenedettaPacilli/movingObstacle/MovingObstacle.cs
@@ -0,0 +1,12 @@
+using System.Timers;
+using System.Windows.Controls;
+
+public class MovingObstacle
+{
+	private Timer _timer;
+	private Image _skin;
+	private int _x;
+	private int _y;
+	private int _direction = -1;
+
+}
