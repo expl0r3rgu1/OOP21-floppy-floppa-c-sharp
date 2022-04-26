9b38a3b81661d04ffd321ceef03e1a18703ef762
Cristina Zoccola <cristina.zoccola01@gmail.com>
2022-04-27 00:02:54 +0200

    Added getter and setter

--- CristinaZoccola/Skin.cs
+++ CristinaZoccola/Skin.cs
@@ -16,6 +16,22 @@ namespace OOP21_floppy_floppa_c_sharp.CristinaZoccola
 			_width = width;
 			_height = height;
 		}
+
+		public string GetName() => _name;
+
+		public void SetName(string name) => _name = name;
+
+		public Image GetImage() => _image;
+
+		public void SetImage(Image image) => _image = image;
+
+		public int GetWidth() => _width;
+
+		public void SetWidth(int width) => _width = width;
+
+		public int GetHeight() => _height;
+
+		public void SetHeight(int height) => _height = height;
 	}
 }
 
