dfbe15ba3a2deafa736dbb119e1e93266e55b163
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-26 23:54:04 +0200

    created new folder named ValentinaPieri and new class ObstacleFactoryImpl

---
+++ ValentinaPieri/ObstacleFactoryImpl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+
+public class ObstacleFactoryImpl
+{
+	public FixedObstacle fixedObstacleFactory(Poition _position, Image _image)
+    {
+        return new FixedObstacle(_position, _image);
+    }
+
+    public MovingObstacle movingObstacleFactory(Poition _position, Image _image)
+    {
+        return new MovingObstacle(_position, _image);
+    }
+}
