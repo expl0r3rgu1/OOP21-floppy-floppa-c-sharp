4f11e2e41a25eb357aa6945f85926d3a1f5b2117
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-27 12:24:27 +0200

    Adde constructor in PricedBackground class

--- BenedettaPacilli/shop/PricedBackground.cs
+++ BenedettaPacilli/shop/PricedBackground.cs
@@ -1,4 +1,11 @@
+using System.Drawing;
+
 public class PricedBackground : Background
 {
     private int _price;
+
+    public PricedBackground(string name, Image image, int price) : base(name, image)
+    {
+        _price = price;
+    }
 }
