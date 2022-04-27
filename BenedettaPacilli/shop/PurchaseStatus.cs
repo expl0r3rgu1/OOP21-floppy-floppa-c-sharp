1461bd2eb814be310aa8fc5faadff90a4008c9ce
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-27 11:53:14 +0200

    Added getters and setters in PurchaseStatus class

--- BenedettaPacilli/shop/PurchaseStatus.cs
+++ BenedettaPacilli/shop/PurchaseStatus.cs
@@ -3,4 +3,28 @@ public class PurchaseStatus<X>
     private X _x;
     private bool _purchased;
 
+    public X GetX()
+    {
+        return _x;
+    }
+
+    public void SetX(int x)
+    {
+        _x = x;
+    }
+
+    public bool IsPurchased()
+    {
+        return _purchased;
+    }
+
+    public void Purchase()
+    {
+        _purchased = true;
+    }
+
+    public void ResetPurchase()
+    {
+        _purchased = false;
+    }
 }
