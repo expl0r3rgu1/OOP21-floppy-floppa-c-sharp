116ab0cc20afc191b158f02607ab7541f17a43a1
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-28 00:23:09 +0200

    created GameSettings

---
+++ ValentinaPieri/GameSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+
+namespace floppy_floppa_c_sharp.OOP21_floppy_floppa_c_sharp.ValentinaPieri
+{
+    class GameSettings
+    {
+        private ScrollingBackground scollingBackground;
+        private Skin skin;
+        private Player player;
+        private readonly Image image = null;
+        private readonly int skin_dimension = 50;
+
+        public Skin Skin { get => skin; set => skin = value; }
+
+        public ScrollingBackground ScollingBackground { get => scollingBackground; set => scollingBackground = value; }
+
+        public Player Player { get => player; set => player = value; }
+
+        public GameSettings()
+        {
+            this.scollingBackground = new ScrollingBackground("Default", image);
+            this.skin = new Skin("Floppa", image, skin_dimension, skin_dimension);
+        }
+
+
+    }
+}
