9362d68c9907bc03b63317e7d8040626fc27e5de
expl0r3rgu1 <salvaddimando@gmail.com>
2022-04-27 14:57:15 +0200

    Created Background constructors

--- SalvatoreAntonioAddimando/Background.cs
+++ SalvatoreAntonioAddimando/Background.cs
@@ -2,11 +2,25 @@ using Utilities.Movable;
 
 namespace InfiniteMap
 {
-    class Background
+    class Background : Movable
     {
         private sealed string name;
         private sealed Image image;
         public string Name { get => name; set => name = value; }
         public Image Image { get => image; set => image = value; }
+
+        public Background(string name, Image image)
+        {
+            super(new Position(0, 0));
+            this.name = name;
+            this.image = image;
+        }
+
+        public Background(string name, Image image, Position position)
+        {
+            super(position);
+            this.name = name;
+            this.image = image;
+        }
     }
 }
