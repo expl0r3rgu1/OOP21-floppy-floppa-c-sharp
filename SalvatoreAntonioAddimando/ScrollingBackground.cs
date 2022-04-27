8cba18bc8ce2b9576877a02c4043a92403553aad
expl0r3rgu1 <salvaddimando@gmail.com>
2022-04-27 17:55:12 +0200

    Created ScrollingBackground constructor

--- SalvatoreAntonioAddimando/ScrollingBackground.cs
+++ SalvatoreAntonioAddimando/ScrollingBackground.cs
@@ -1,8 +1,17 @@
+using System.Windows.Forms;
+using System.Drawing;
+
 namespace InfiniteMap
 {
     class ScrollingBackground
     {
         private readonly Background backOne;
         private readonly Background backTwo;
+
+        public ScrollingBackground(string name, Image image)
+        {
+            backOne = new Background(name, image);
+            backTwo = new Background(name, image, new Position(1920, 0));
+        }
     }
 }
