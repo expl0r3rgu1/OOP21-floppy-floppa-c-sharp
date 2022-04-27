08cd90d7375070040a12387c6dee3a0bddee132b
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-28 01:15:34 +0200

    created Malus

---
+++ ValentinaPieri/Malus.cs
@@ -0,0 +1,23 @@
+﻿using floppy_floppa_c_sharp.OOP21_floppy_floppa_c_sharp.test;
+using System;
+
+namespace floppy_floppa_c_sharp.OOP21_floppy_floppa_c_sharp.ValentinaPieri
+{
+    public abstract class Malus : Movable
+    {
+        private Skin skin;
+
+        public Skin Skin => skin;
+
+        protected Malus(Position position, Skin skin) : base(position) => this.skin = skin;
+
+        public abstract void ChangeState();
+
+        public void MoveOffScreen()
+        {
+            Position.X = -skin.Width;
+            Position.Y = Position.Y;
+        }
+
+    }
+}
