8c5a73345aba0d20a6bc418db278e64c28ffbac0
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-27 11:43:50 +0200

    Added fields in Shop class

--- BenedettaPacilli/shop/Shop.cs
+++ BenedettaPacilli/shop/Shop.cs
@@ -1,4 +1,15 @@
+using System.Collections.Generic;
+using System.IO;
+
 public class Shop
 {
-
+	private int _skinsNum;
+	private int _sceneriesNum;
+	private int _coins;
+	private sealed List<string> _skinInitialize;
+	private sealed List<string> _backgroundInitialize;
+	private sealed List<int> _prices;
+	//private sealed List<PurchaseStatus<PricedSkin>> _skins;
+	//private sealed List<PurchaseStatus<PricedBackground>> _sceneries;
+	private sealed File _savingsFile;
 }
