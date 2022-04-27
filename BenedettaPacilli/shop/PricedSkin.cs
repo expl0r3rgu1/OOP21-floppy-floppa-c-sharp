77740091f25c8c85120b533d05ece4b8064770c2
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-27 12:10:23 +0200

    Adde constructor in PricedSkin class

--- BenedettaPacilli/shop/PricedSkin.cs
+++ BenedettaPacilli/shop/PricedSkin.cs
@@ -1,4 +1,11 @@
+using System.Drawing;
+
 public class PricedSkin : Skin
 {
     private int _price;
+
+    public PricedSkin(string name, Image image, int width, int height, int price):base(name, image, width, height)
+    {
+        _price = price;
+    }
 }
