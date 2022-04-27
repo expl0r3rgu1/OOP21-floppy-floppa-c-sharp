4917e1bfd0704c545935dd05905cc5aed60b3f65
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-28 01:17:19 +0200

    created CoinsReducer

---
+++ ValentinaPieri/CoinsReducer.cs
@@ -0,0 +1,34 @@
+﻿using floppy_floppa_c_sharp.OOP21_floppy_floppa_c_sharp.test;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace floppy_floppa_c_sharp.OOP21_floppy_floppa_c_sharp.ValentinaPieri
+{
+    public class CoinsReducer : Malus
+    {
+        public const int MovingFactor = 2;
+
+        public CoinsReducer(Position position, Skin skin) : base(position, skin);
+
+        public override void ChangeState()
+        {
+            MoveOffScreen();
+        }
+
+        private void UpdatePositionX()
+        {
+            Position.X = Position.X - 3 * MovingFactor;
+            Position.Y = Position.Y;
+        }
+
+        public override void Animate(RibbonElementPaintEventArgs ribbonPaintEventArgs)
+        {
+            ribbonPaintEventArgs.Graphics.DrawImage(skin.Image, Position.X, Position.Y, skin.Width, skin.Height);
+
+            UpdatePositionX();
+        }
+
+    }
+}
