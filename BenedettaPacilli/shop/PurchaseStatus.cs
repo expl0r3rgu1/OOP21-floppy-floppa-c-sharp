ad71f8ba0726f21d71cd4e3de84de74670a5bf87
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-27 16:16:48 +0200

    Changed constructors

--- BenedettaPacilli/shop/PurchaseStatus.cs
+++ BenedettaPacilli/shop/PurchaseStatus.cs
@@ -3,28 +3,12 @@ public class PurchaseStatus<X>
     private X _x;
     private bool _purchased;
 
-    public X GetX()
-    {
-        return _x;
-    }
-
-    public void SetX(int x)
-    {
-        _x = x;
-    }
-
-    public bool IsPurchased()
-    {
-        return _purchased;
-    }
-
-    public void Purchase()
-    {
-        _purchased = true;
-    }
+    public X X { get => _x; set => _x = value; }
+    
+    public bool Purchased { get => _purchased; set => _purchased = true; }
 
     public void ResetPurchase()
     {
-        _purchased = false;
+        Purchased = false;
     }
 }
