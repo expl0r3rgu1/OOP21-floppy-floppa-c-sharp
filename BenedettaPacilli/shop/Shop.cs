5452718619756e0e671e986320de987ef57d8710
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-27 12:38:58 +0200

    Added constructor in Shop class

--- BenedettaPacilli/shop/Shop.cs
+++ BenedettaPacilli/shop/Shop.cs
@@ -9,7 +9,33 @@ public class Shop
 	private sealed List<string> _skinInitialize;
 	private sealed List<string> _backgroundInitialize;
 	private sealed List<int> _prices;
-	//private sealed List<PurchaseStatus<PricedSkin>> _skins;
-	//private sealed List<PurchaseStatus<PricedBackground>> _sceneries;
+	private sealed List<PurchaseStatus<PricedSkin>> _skins;
+	private sealed List<PurchaseStatus<PricedBackground>> _sceneries;
 	private sealed File _savingsFile;
+
+	public Shop()
+    {
+		_coins = 0;
+		_skins = new List<PurchaseStatus<PricedSkin>>();
+		_sceneries = new List<PurchaseStatus<PricedBackground>>();
+		_skinInitialize = new List<string>
+		{
+			"Floppa", "Sogga", "Capibara", "Quokka", "Buding",
+		};
+		_backgroundInitialize = new List<string>
+		{
+			"Classic", "Beach", "Woods", "Space", "NeonCity",
+		};
+
+		_skinsNum = _skinInitialize.Count;
+		_sceneriesNum = _backgroundInitialize.Count;
+
+		_prices = new List<int>
+		{
+			0, 50, 100, 200, 500,
+		};
+
+		_savingsFile = new File(); //TODO
+
+	}
 }
