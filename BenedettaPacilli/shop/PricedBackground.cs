5ede28b131e60b59b68a3e51f187f2af64a97cce
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-28 15:40:43 +0200

    Fixed indentation

--- BenedettaPacilli/shop/PricedBackground.cs
+++ BenedettaPacilli/shop/PricedBackground.cs
@@ -3,26 +3,26 @@ using InfiniteMap;
 
 namespace ShopSpace
 {
-public class PricedBackground : Background
-{
-    private int price;
-
-    public int Price { get => price; set => price = value; }
-
-    public PricedBackground(string name, Image image, int price) : base(name, image)
+    public class PricedBackground : Background
     {
-        Price = price;
-    }
+        private int price;
 
-    public bool Equals(object obj)
-    {
-        PricedBackground other = (PricedBackground)obj;
-        return base.Equals(other) && Price == other.Price;
-    }
+        public int Price { get => price; set => price = value; }
 
-    public int GetHashCode()
-    {
-        return base.GetHashCode();
+        public PricedBackground(string name, Image image, int price) : base(name, image)
+        {
+            Price = price;
+        }
+
+        public bool Equals(object obj)
+        {
+            PricedBackground other = (PricedBackground)obj;
+            return base.Equals(other) && Price == other.Price;
+        }
+
+        public int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
-}
 }
