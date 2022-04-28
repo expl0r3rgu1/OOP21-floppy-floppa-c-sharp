143572c65d591350a1052a4b8b9608099a1d3214
expl0r3rgu1 <salvaddimando@gmail.com>
2022-04-28 18:41:05 +0200

    Created documentation for Player

--- SalvatoreAntonioAddimando/Player.cs
+++ SalvatoreAntonioAddimando/Player.cs
@@ -2,22 +2,33 @@
 
 namespace LeaderboardSpace
 {
+    /// <summary>
+    /// The user that is playing the game
+    /// </summary>
     public class Player : IComparable
     {
         private readonly string nickname;
         private int personalBest;
 
+        /// <summary>
+        /// The nickname of the Player
+        /// </summary>
         public string Nickname
         {
             get { return nickname; }
         }
 
+        /// <summary>
+        /// The best score of the Player
+        /// </summary>
         public int PersonalBest
         {
             get { return personalBest; }
             set { personalBest = value; }
         }
 
+        /// <param name="nickname">The nickname of the Player</param>
+        /// <param name="personalBest">The best score of the Player</param>
         public Player(string nickname, int personalBest)
         {
             this.nickname = nickname;
@@ -27,11 +38,13 @@ namespace LeaderboardSpace
             }
         }
 
+        /// <inheritdoc/>
         public override string ToString()
         {
             return nickname + "," + personalBest;
         }
 
+        /// <inheritdoc/>
         public int CompareTo(object obj)
         {
 			if (obj == null) return 1;
