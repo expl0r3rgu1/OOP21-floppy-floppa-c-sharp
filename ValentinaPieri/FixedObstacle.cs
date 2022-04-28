5eb449c9bfcb10fe8f14bad6d61f7076f1da50ac
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-28 16:22:38 +0200

    added comments in FixedObstacle

--- ValentinaPieri/FixedObstacle.cs
+++ ValentinaPieri/FixedObstacle.cs
@@ -4,6 +4,9 @@ using System.Windows.Forms;
 
 namespace ObstacleFactory
 {
+	/// <summary>
+	/// A class that implements a fixed Obstacle
+	/// </summary>
 	public class FixedObstacle : Movable
 	{
 		private readonly Skin skin;
@@ -12,19 +15,29 @@ namespace ObstacleFactory
         private readonly int screen_size_width = 1080;
         private readonly int screen_size_height = 980;
 
+		/// <summary>
+		/// The Skin of the entity
+		/// </summary>
 		public Skin Skin => skin;
 
+		/// <param name="position">the obstacle initial Position</param>
+		/// <param name="skin">the obstacle Skin</param>
 		public FixedObstacle(Position position, Skin skin) : base(position)
 		{
 			this.skin = skin;
 		}
 
+		/// <summary>
+		/// UpdatePosition is a method that as its name says updates the position, the X,
+		/// of the fixedobstacle
+		/// </summary>
 		private void UpdatePosition()
 		{
 			Position.X = Position.X - MovingFactor;
 			Position.Y = Position.Y;
 		}
 
+		/// <inheritdoc />
 		public override void Animate(RibbonElementPaintEventArgs ribbonPaintEventArgs)
 		{
 			ribbonPaintEventArgs.Graphics.DrawImage(skin.Image, Position.X, Position.Y + (int)space_between_pipes / 2, screen_size_width / 10, screen_size_height - (Position.Y + (int)space_between_pipes / 2));
@@ -32,6 +45,7 @@ namespace ObstacleFactory
 			UpdatePosition();
 		}
 
+		/// <inheritdoc />
 		public override bool Equals(object obj)
 		{
 			FixedObstacle other = obj as FixedObstacle;
@@ -44,6 +58,7 @@ namespace ObstacleFactory
 			return base.Equals(obj) && skin.Equals(other.skin);
 		}
 
+		/// <inheritdoc />
 		public override int GetHashCode() => base.GetHashCode();
 
 	}
