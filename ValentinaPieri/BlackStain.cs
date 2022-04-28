2f7657fdb73237a3ccd3aff30216429b26463b71
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-28 16:20:37 +0200

    added comments in BlackStain

--- ValentinaPieri/BlackStain.cs
+++ ValentinaPieri/BlackStain.cs
@@ -1,9 +1,14 @@
-﻿using floppy_floppa_c_sharp.OOP21_floppy_floppa_c_sharp.test;
+﻿using Utilities;
 using System.Timers;
 using System.Windows.Forms;
 
 namespace floppy_floppa_c_sharp.OOP21_floppy_floppa_c_sharp.ValentinaPieri
 {
+	/// <summary>
+	/// A class that extends Malus class and implements an entity that makes appear a
+	/// stain on the screen, blocking the players vision of parts of the screen, 
+	/// every time they hit this malus
+	/// </summary>
 	public class BlackStain : Malus
 	{
 		private bool collided = false;
@@ -14,12 +19,15 @@ namespace floppy_floppa_c_sharp.OOP21_floppy_floppa_c_sharp.ValentinaPieri
 		private readonly int screen_size_width = 1080;
 		private readonly int screen_size_height = 980;
 
-        public BlackStain(Position position, Skin skin) : base(position, skin)
+		/// <param name="position">the BlackStain initial Position</param>
+		/// <param name="skin">the BlackStain Skin</param>
+		public BlackStain(Position position, Skin skin) : base(position, skin)
         {
 			timer = new Timer(300);
-			timer.Elapsed += new ElapsedEventHandler(ActionPerformed);
+			timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
 		}
-	
+
+		/// <inheritdoc />
 		public override void ChangeState()
 		{
 			timer.Start();
@@ -28,12 +36,18 @@ namespace floppy_floppa_c_sharp.OOP21_floppy_floppa_c_sharp.ValentinaPieri
 			Position.Y = Position.Y;
 		}
 
+		/// <summary>
+		/// The method gives the BlackStain malus a new Position that leaves the Y
+		/// position unchanged, while the X position decreases so that the object moves
+		/// from right to left
+		/// </summary>
 		private void UpdatePositionX()
 		{
 			Position.X = Position.X - 3 * MovingFactor;
 			Position.Y = Position.Y;
 		}
 
+		/// <inheritdoc />
 		public override void Animate(RibbonElementPaintEventArgs ribbonPaintEventArgs)
 		{
 			ribbonPaintEventArgs.Graphics.DrawImage(Skin.Image, Position.X, Position.Y, Skin.Width, Skin.Height);
@@ -46,7 +60,12 @@ namespace floppy_floppa_c_sharp.OOP21_floppy_floppa_c_sharp.ValentinaPieri
 			}
 		}
 
-		public void ActionPerformed(object sender, ElapsedEventArgs e)
+		/// <summary>
+		/// OnTimedEvent stops the timer
+		/// <summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		public void OnTimedEvent(object sender, ElapsedEventArgs e)
 		{
 			this.collided = false;
 			this.timer.Stop();
