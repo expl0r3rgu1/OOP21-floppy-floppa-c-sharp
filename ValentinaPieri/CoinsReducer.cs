a5d90b44d853fb75449688360f50d564e1c13ee3
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-28 16:20:56 +0200

    added comments in CoinsReducer

--- ValentinaPieri/CoinsReducer.cs
+++ ValentinaPieri/CoinsReducer.cs
@@ -3,25 +3,38 @@ using System.Windows.Forms;
 
 namespace floppy_floppa_c_sharp.OOP21_floppy_floppa_c_sharp.ValentinaPieri
 {
+    /// <summary>
+    /// A class that extends Malus class and implements an entity that randomly takes
+    /// away coins to the main character every time they hit this malus
+	/// </summary>
     public class CoinsReducer : Malus
     {
         private const int MovingFactor = 2;
         private int PlayPanel_reducerTimes = 0;
 
+        /// <param name="position">the CoinsReducer initial Position</param>
+		/// <param name="skin">the CoinsReducer Skin</param>
         public CoinsReducer(Position position, Skin skin) : base(position, skin) { }
 
+        /// <inheritdoc />
         public override void ChangeState()
         {
             PlayPanel_reducerTimes++;
             MoveOffScreen();
         }
 
+        /// <summary>
+		/// The method gives the CoinsReducer malus a new Position that leaves the Y
+		/// position unchanged, while the X position decreases so that the object moves
+		/// from right to left
+		/// </summary>
         private void UpdatePositionX()
         {
             Position.X = Position.X - 3 * MovingFactor;
             Position.Y = Position.Y;
         }
 
+        /// <inheritdoc />
         public override void Animate(RibbonElementPaintEventArgs ribbonPaintEventArgs)
         {
             ribbonPaintEventArgs.Graphics.DrawImage(Skin.Image, Position.X, Position.Y, Skin.Width, Skin.Height);
