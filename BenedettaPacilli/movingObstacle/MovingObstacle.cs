7bdf41f73dc144ed66367f688433e92de7e0c98d
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-04-28 15:41:30 +0200

    Fixed indentation

--- BenedettaPacilli/movingObstacle/MovingObstacle.cs
+++ BenedettaPacilli/movingObstacle/MovingObstacle.cs
@@ -3,52 +3,51 @@ using System.Windows.Forms;
 using Utilities;
 
 namespace ObstacleFactory {
-public class MovingObstacle : Movable
-{
-	private Timer timer;
-	private Skin skin;
-	private int direction = -1;
-	private const int movingFactor = 2;
-
-    public Skin Skin { get => skin; set => skin = value; }
-
-    public MovingObstacle(Position position, Skin skin):base(position)
+	public class MovingObstacle : Movable
 	{
-		skin = skin;
+		private Timer timer;
+		private Skin skin;
+		private int direction = -1;
+		private const int movingFactor = 2;
 
-		timer = new Timer(1000);
-		timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-	}
+		public Skin Skin { get => skin; set => skin = value; }
 
-	private void UpdatePosition()
-    {
-		Position.X = Position.X - 3 * movingFactor;
-		Position.Y = Position.Y + direction * movingFactor;
-    }
+		public MovingObstacle(Position position, Skin skin):base(position)
+		{
+			skin = skin;
 
-	private override Animate(RibbonElementPaintEventArgs ribbonElementPaintEventArgs)
-	{
-		ribbonElementPaintEventArgs.Graphics.DrawImage(skin.Image, Position.X, Position.Y, skin.Width, skin.Height);
+			timer = new Timer(1000);
+			timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+		}
 
-		UpdatePosition();
-	}
+		private void UpdatePosition()
+		{
+			Position.X = Position.X - 3 * movingFactor;
+			Position.Y = Position.Y + direction * movingFactor;
+		}
+
+		private override Animate(RibbonElementPaintEventArgs ribbonElementPaintEventArgs)
+		{
+			ribbonElementPaintEventArgs.Graphics.DrawImage(skin.Image, Position.X, Position.Y, skin.Width, skin.Height);
+
+			UpdatePosition();
+		}
 	
-	private void OnTimedEvent(object sender, ElapsedEventArgs e)
-	{
-		direction = -direction;
-		timer.Stop();
+		private void OnTimedEvent(object sender, ElapsedEventArgs e)
+		{
+			direction = -direction;
+			timer.Stop();
+		}
+
+		public bool Equals(object obj)
+		{
+			MovingObstacle other = (MovingObstacle)obj;
+			return base.Equals(other) && this.skin = other.skin;
+		}
+
+		public int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
 	}
-
-    public bool Equals(object obj)
-    {
-		MovingObstacle other = (MovingObstacle)obj;
-        return base.Equals(other) && this.skin = other.skin;
-    }
-
-    public int GetHashCode()
-    {
-        return base.GetHashCode();
-    }
-
-}
 }
