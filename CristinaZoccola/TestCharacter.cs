e3e2ad7b4a2818cf0ba135c93772ef48f94de790
Cristina <cristina.zoccola01@gmail.com>
2022-04-29 16:03:18 +0200

    Added fields

--- CristinaZoccola/TestCharacter.cs
+++ CristinaZoccola/TestCharacter.cs
@@ -1,8 +1,35 @@
 ﻿using NUnit.Framework;
+using CharacterSpace;
+using ObstacleFactory;
+using System.Drawing;
+using Utilities;
+using System.Collections.Generic;
+
 
 namespace Test
 {
+    [TestFixture]
     class TestCharacter
     {
+        private const int screenWidth = 1920;
+        private const int screenHeight = 1080;
+        private Character characterCollideUpperPipe;
+        private Character characterCollideLowerPipe;
+        private Character characterCollideMovingEntity;
+        private Character characterCollideUpperBorder;
+        private Character characterCollideLowerBorder;
+        private List<FixedObstacle> fixedObstacleList;
+        private FixedObstacle fixedObstacle;
+        private List<MovingObstacle> movingObstacleList;
+        private MovingObstacle movingObstacle;
+        private Image imagePlaceHolder;
+        private Skin skin;
+        private Position fixedObstaclePosition;
+        private Position movingObstaclePosition;
+        private Position characterCollideUpperPipePosition;
+        private Position characterCollideLowerPipePosition;
+        private Position characterCollideMovingEntityPosition;
+        private Position characterCollideUpperBorderPosition;
+        private Position characterCollideLowerBorderPosition;
     }
 }
