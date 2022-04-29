7616f3d4b30bcfa499f9ab94d2a495a1065b9855
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-29 17:22:19 +0200

    added variable in TestFactoryObstacleImpl

--- ValentinaPieri/TestFactoryObstacleImpl.cs
+++ ValentinaPieri/TestFactoryObstacleImpl.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using ObstacleFactory;
+using System.Drawing;
 using Utilities;
 
 namespace Test
@@ -6,6 +8,15 @@ namespace Test
     [TestFixture]
     public class TestFactoryObstacleImpl
     {
+        private const int screenSizeWidth = 1080;
+        private const int screenSizeHeight = 980;
+        private const Image image = null;
+        private const int skinDimension = 50;
+
+        private readonly Position position = new Position(screenSizeWidth, screenSizeHeight);
+        private readonly Skin skinFixedObstacle = new Skin("pipe", image, skinDimension, skinDimension);
+        readonly private Skin skinMovingObstacle = new Skin("Bingus", image, skinDimension, skinDimension);
+
         
 
     }
