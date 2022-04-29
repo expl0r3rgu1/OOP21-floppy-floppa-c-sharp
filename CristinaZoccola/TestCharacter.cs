6529c999c2ab985a1ed4c33821b2e21c209186e3
Cristina <cristina.zoccola01@gmail.com>
2022-04-29 16:37:56 +0200

    Created SetUp() method

--- CristinaZoccola/TestCharacter.cs
+++ CristinaZoccola/TestCharacter.cs
@@ -13,6 +13,8 @@ namespace Test
     {
         private const int screenWidth = 1920;
         private const int screenHeight = 1080;
+        private const int skinWidth = 20;
+        private const int skinHeight = 20;
         private Character characterCollideUpperPipe;
         private Character characterCollideLowerPipe;
         private Character characterCollideMovingEntity;
@@ -31,5 +33,41 @@ namespace Test
         private Position characterCollideMovingEntityPosition;
         private Position characterCollideUpperBorderPosition;
         private Position characterCollideLowerBorderPosition;
+
+        [SetUp]
+        public void SetUp()
+        {
+            imagePlaceHolder = null;
+            skin = new Skin("skin", imagePlaceHolder, skinWidth, skinHeight);
+
+            fixedObstaclePosition = new Position(screenWidth / 2, screenHeight / 2);
+            fixedObstacle = new ObstacleFactoryImpl().FixedObstacleFactory(fixedObstaclePosition, skin);
+            fixedObstacleList = new List<FixedObstacle>()
+            {
+                fixedObstacle
+            };
+
+            movingObstaclePosition = new Position(screenWidth / 2, screenHeight / 2);
+            movingObstacle = new ObstacleFactoryImpl().MovingObstacleFactory(movingObstaclePosition, skin);
+            movingObstacleList = new List<MovingObstacle>()
+            {
+                movingObstacle
+            };
+
+            characterCollideUpperPipePosition = new Position(screenWidth / 2, screenHeight / 5);
+            characterCollideUpperPipe = new Character(characterCollideUpperPipePosition, skin);
+
+            characterCollideLowerPipePosition = new Position(screenWidth / 2, screenHeight * 4 / 5);
+            characterCollideLowerPipe = new Character(characterCollideLowerPipePosition, skin);
+
+            characterCollideMovingEntityPosition = new Position(screenWidth + movingObstacleList[0].Skin.Width / 2, screenHeight + movingObstacleList[0].Skin.Height / 2);
+            characterCollideMovingEntity = new Character(characterCollideMovingEntityPosition, skin);
+
+            characterCollideUpperBorderPosition = new Position(skinWidth / 2, -1);
+            characterCollideUpperBorder = new Character(characterCollideUpperBorderPosition, skin);
+
+            characterCollideLowerBorderPosition = new Position(screenWidth / 2, screenHeight + 1);
+            characterCollideLowerBorder = new Character(characterCollideLowerBorderPosition, skin);
+        }
     }
 }
