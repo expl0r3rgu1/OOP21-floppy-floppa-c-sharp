eb94b2480aacfc82924671a5dd22fdd857b6a720
ValentinaPieri <valentina.pieri5@studio.unibo.it>
2022-04-29 18:20:20 +0200

    changed image in TestFactoryObstacleImpl

--- ValentinaPieri/TestFactoryObstacleImpl.cs
+++ ValentinaPieri/TestFactoryObstacleImpl.cs
@@ -8,21 +8,25 @@ namespace Test
     [TestFixture]
     public class TestFactoryObstacleImpl
     {
+        static void Main() { }
+
         private const int screenSizeWidth = 1080;
         private const int screenSizeHeight = 980;
-        private const Image image = null;
+        private const Image image = Image.FromFile("Blackstain.png");
         private const int skinDimension = 50;
 
         private readonly Position position = new Position(screenSizeWidth, screenSizeHeight);
-        private readonly Skin skinFixedObstacle = new Skin("pipe", image, skinDimension, skinDimension);
-        readonly private Skin skinMovingObstacle = new Skin("Bingus", image, skinDimension, skinDimension);
+        private readonly Skin skinFixedObstacle = new Skin("Blackstain.png", image, skinDimension, skinDimension);
+        //readonly private Skin skinMovingObstacle = new Skin("Bingus", image, skinDimension, skinDimension);
 
+        
+        [Test]
         public void TestFactoryFixedObstcle()
         {
             ObstacleFactoryImpl factory = new ObstacleFactoryImpl();
             FixedObstacle fixedO = new FixedObstacle(position, skinFixedObstacle);
 
-            
+            Assert.True(fixedO.Equals(factory.FixedObstacleFactory(position, skinFixedObstacle)));
         }
 
     }
