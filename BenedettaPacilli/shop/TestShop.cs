70c0688a4384c4c79e2a07dbf6ade055a4fe4c05
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2022-05-03 10:23:51 +0200

    Added FileReading method for the testing class of the Shop class

--- BenedettaPacilli/shop/TestShop.cs
+++ BenedettaPacilli/shop/TestShop.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace ShopSpace
 {
+    [TestFixture]
     public class TestShop
     {
+        private const int ENOUGH_COINS = 1000;
+        private const int NOT_ENOUGH_COINS = 0;
 
+        [Test]
+        public void FileReading()
+        {
+            Shop shop = new Shop();
+
+            for(int i = 0; i < shop.SkinsNum; i++)
+            {
+                if (i == 0)
+                {
+                    Assert.IsTrue(shop.Skins[i].Purchased)
+                } else
+                {
+                    Assert.IsFalse(shop.Skins[i].Purchased);
+                }
+            }
+
+            for (int i = 0; i < shop.SceneriesNum; i++)
+            {
+                if (i == 0)
+                {
+                    Assert.IsTrue(shop.Sceneries[i].Purchased);
+                } else
+                {
+                    Assert.IsFalse(shop.Sceneries[i].Purchased);
+                }
+            }
+        }
     }
 }
