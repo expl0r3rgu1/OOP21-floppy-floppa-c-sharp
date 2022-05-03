d19e17fe86cd45ae692f8ddbc2ec2b873d28fc2b
expl0r3rgu1 <salvaddimando@gmail.com>
2022-05-03 18:56:15 +0200

    Created createSavingsFile method in TestLeaderboard

--- SalvatoreAntonioAddimando/TestLeaderboard.cs
+++ SalvatoreAntonioAddimando/TestLeaderboard.cs
@@ -1,11 +1,20 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using NUnit.Framework;
 
 namespace Test
 {
     [TestFixture]
     public class TestLeaderboard
     {
+        private const string savingsFilePath = "savings";
+        private const string savingsFileStartContent = "0\n0,0,0,0,0\n0,0,0,0,0";
 
-
+        private void CreateSavingsFile()
+        {
+            File.Create(savingsFilePath);
+            StreamWriter sw = new StreamWriter(savingsFilePath);
+            sw.WriteLine(savingsFileStartContent);
+            sw.Close();
+        }
     }
 }
